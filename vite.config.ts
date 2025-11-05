00eba6f0e6684245eb9bcb58c123b5245bb54f2b
Sam <102683241+aboringpenguin@users.noreply.github.com>
2025-11-05 16:45:57 +0100

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -1,23 +1,11 @@
-import path from 'path';
-import { defineConfig, loadEnv } from 'vite';
-import react from '@vitejs/plugin-react';
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
 
-export default defineConfig(({ mode }) => {
-    const env = loadEnv(mode, '.', '');
-    return {
-      server: {
-        port: 3000,
-        host: '0.0.0.0',
-      },
-      plugins: [react()],
-      define: {
-        'process.env.API_KEY': JSON.stringify(env.GEMINI_API_KEY),
-        'process.env.GEMINI_API_KEY': JSON.stringify(env.GEMINI_API_KEY)
-      },
-      resolve: {
-        alias: {
-          '@': path.resolve(__dirname, '.'),
-        }
-      }
-    };
-});
+// https://vitejs.dev/config/
+export default defineConfig({
+  plugins: [react()],
+  // IMPORTANT: You must replace 'your-repo-name' with the name of your GitHub repository.
+  // For example, if your repo URL is https://github.com/user/my-pokedex,
+  // you should set base to '/my-pokedex/'.
+  base: '/PokeStats/',
+})
